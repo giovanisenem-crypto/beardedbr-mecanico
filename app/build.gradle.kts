0005a1bf79464cb08fbbe82df960e7752f6c5c6f
giovanisenem-crypto <giovanisenem@gmail.com>
2026-09-10 06:52:17 -0300

    Atualizar versão para 4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
         applicationId = "br.com.beardedbr.mecanico"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
     }
 }
