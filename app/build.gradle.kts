4f991b72de296a5e842d8c1e9ff414cc23872c7d
giovanisenem-crypto <giovanisenem@gmail.com>
2026-09-10 06:04:54 -0300

    Adicionar app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "br.com.beardedbr.mecanico"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "br.com.beardedbr.mecanico"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
