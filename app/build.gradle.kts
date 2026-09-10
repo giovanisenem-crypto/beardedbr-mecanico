eed9a91cc836060c03fd549cce65c3e5854a2c50
giovanisenem-crypto <giovanisenem@gmail.com>
2026-09-10 07:18:09 -0300

    Atualizar versão para 5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
         applicationId = "br.com.beardedbr.mecanico"
         minSdk = 23
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "5.0"
     }
 }
