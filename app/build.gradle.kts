08ab3b13e770e4206d3274067fe7742ac2f1a485
giovanisenem-crypto <giovanisenem@gmail.com>
2026-09-10 07:34:59 -0300

    Atualizar versão para 6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
         applicationId = "br.com.beardedbr.mecanico"
         minSdk = 23
         targetSdk = 35
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
     }
 }
