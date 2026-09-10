b321d86406fc275064114ef4a1a34464bb866f1f
giovanisenem-crypto <giovanisenem@gmail.com>
2026-09-10 12:56:59 -0300

    Atualizar versão para 9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
         applicationId = "br.com.beardedbr.mecanico"
         minSdk = 23
         targetSdk = 35
-        versionCode = 8
-        versionName = "8.0"
+        versionCode = 9
+        versionName = "9.0"
     }
 }
