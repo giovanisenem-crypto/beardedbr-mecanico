830d2318e4708d0072c20bcc8c3bbec281302b16
giovanisenem-crypto <giovanisenem@gmail.com>
2026-09-10 14:01:35 -0300

    Atualizar versão para 12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
         applicationId = "br.com.beardedbr.mecanico"
         minSdk = 23
         targetSdk = 35
-        versionCode = 11
-        versionName = "11.0"
+        versionCode = 12
+        versionName = "12.0"
     }
 }
