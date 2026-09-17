cc8d7954209db9e179379cccc500e7cc21579e3c
giovanisenem-crypto <giovanisenem@gmail.com>
2026-09-16 21:44:37 -0300

    Adiciona animacoes lendarias 3D na V18

--- forja-de-aco/app/build.gradle.kts
+++ forja-de-aco/app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins { id("com.android.application") }
 
 android { namespace = "br.com.beardedbr.forjadeaco"; compileSdk = 35
- defaultConfig { applicationId = "br.com.beardedbr.forjadeaco"; minSdk = 23; targetSdk = 35; versionCode = 17; versionName = "1.0" }
+ defaultConfig { applicationId = "br.com.beardedbr.forjadeaco"; minSdk = 23; targetSdk = 35; versionCode = 18; versionName = "1.0" }
 }
